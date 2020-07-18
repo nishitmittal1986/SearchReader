871751e90ab3dcaf63a01b931dce11f8432da95c
Nishit Mittal <mittalsecond@gmail.com>
2020-07-18 16:45:55 +1000

    Search Program for Infotrack

---
+++ SearchReadV1/SearchReader.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SearchReader.aspx.cs" Inherits="SearchReadV1.SearchReader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        
+        <asp:RadioButton ID="GoogleRdb" GroupName="SearchEngine" runat="server" Text="Google" Checked="true" />
+        <asp:RadioButton ID="BingRdb" GroupName="SearchEngine" runat="server"  Text="Bing"/>
+        <br />
+        <asp:TextBox ID="SearchTxt" runat="server" Width="188px" Text="online title search" ></asp:TextBox>
+        <br />        
+        <asp:Button ID="RunBtn" runat="server" Text="Run" OnClick="RunBtn_Click" />
+        <br />        
+        <asp:Label ID="ShowLbl" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ SearchReadV1/SearchReader.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SearchReadV1
+{
+    public partial class SearchReader : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+        }
+
+        protected void RunBtn_Click(object sender, EventArgs e)
+        {
+            string GoogleAdd = "https://www.google.com.au/search";
+            string BingAdd = "https://www.bing.com/search";
+            string SearchThis = SearchTxt.Text;
+
+            string uriString = GoogleRdb.Checked ? GoogleAdd : BingAdd;  
+                     
+
+            WebClient webClient = new WebClient();
+
+            NameValueCollection nameValueCollection = new NameValueCollection();
+            nameValueCollection.Add("q", SearchThis);
+
+            webClient.QueryString.Add(nameValueCollection);
+            ShowLbl.Text = webClient.DownloadString(uriString);
+        }
+    }
+}
